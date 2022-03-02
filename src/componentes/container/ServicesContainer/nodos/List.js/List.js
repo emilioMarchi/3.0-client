8da881f6f11fb68997bd7fc91e1fb9e49ea19574
emilioMarchi <em.98.nmynm@gmail.com>
2022-03-02 08:51:35 -0300

    dev cards icon services-container

--- src/componentes/container/ServicesContainer/nodos/List.js/List.js
+++ src/componentes/container/ServicesContainer/nodos/List.js/List.js
@@ -13,19 +13,19 @@ const categoriesData = [
         id: 0,
         title: 'Desarrollo Web',
         itemDescription: ['Landing page', 'Web business', 'E commerce'],
-        img: './img/icons/pagina-web.png'
+        img: './img/icons/web.png'
     },
     {
         id: 1,
         title: 'Marketing Digital',
         itemDescription: ['Estudio de mercado', 'Plan de marketing', 'Distribución de contenido'],
-        img: './img/icons/crecimiento-social.png'
+        img: './img/icons/marketing.png'
     },
     {
         id: 2,
         title: 'Contenido de marca',
         itemDescription: ['Gráfica corporativa', 'Condenido multimedia', 'Fotografía corporativa'],
-        img: './img/icons/contenido.png'
+        img: './img/icons/media.png'
     }
 ]
 export default function List() {
