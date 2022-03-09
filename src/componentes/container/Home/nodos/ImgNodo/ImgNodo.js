3d7358d1e4df6b391ada89ae23724c2d80268828
emilioMarchi <em.98.nmynm@gmail.com>
2022-03-09 13:34:39 -0300

    nuevo commit

--- src/componentes/container/Home/nodos/ImgNodo/ImgNodo.js
+++ src/componentes/container/Home/nodos/ImgNodo/ImgNodo.js
@@ -2,8 +2,7 @@ import React from 'react'
 import './ImgNodo.css'
 export default function ImgNodo(){
     return(
-        <div className='img-nodo col-xs-12 col-4'>
-            <img src='./img/home-gif-3.gif' />
+        <div className='img-nodo col-xs-12 col-6'>
         </div>
     )
 }
--- src/componentes/container/Home/nodos/TextNoodo/TextNodo.js
+++ src/componentes/container/Home/nodos/TextNoodo/TextNodo.js
@@ -3,7 +3,7 @@ import './TextNodo.css'
 
 export default function TextNodo() {
     return(
-        <div className='text-nodo col-xs-12 col-md'>
+        <div className='text-nodo col-xs-12 col-md-6'>
             <h1>Construye el <b>ecosistema digital</b> de tu marca</h1>
             <h2>Diseñamos y desarrollamos con vos el <b>ecosistema digital</b> que tu negocio necesita para comunicarse con el mundo</h2>
         </div>
